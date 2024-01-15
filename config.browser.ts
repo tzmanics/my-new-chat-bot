8ca1269757f4eee8cb1f80be024f7a8783595bef
Tara Z. Manicsic <tzmanics@gmail.com>
2024-01-15 07:00:19 -0500

    changes promps and welcome msg

--- config.browser.ts
+++ config.browser.ts
@@ -1,4 +1,4 @@
-import { samplePhrases } from "./prompts/movie-critic";
+import { samplePhrases } from "./prompts/respectful-parenting";
 // import { samplePhrases } from "./prompts/tour-guide";
 
 export const appConfig = {
